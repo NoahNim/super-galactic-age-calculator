19694b451c884509f485f2d2097ecccf4ba0a81e
Noah Medoff <noahmedoff@Noahs-MacBook-Pro.local>
2018-05-25 13:52:51 -0700

    add test to find age difference in seconds. all tests and functions passing

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -2,14 +2,17 @@ import { galactic } from './../src/age.js'
 
 describe('galactic', function() {
 
+  it('should test difference by age in seconds', function(){
+    let testInput = new galactic('1990-04-21', 70, 28);
+    let testInputAge = testInput.ageInSeconds();
+    expect(testInputAge - testInputAge).toEqual(testInputAge - testInputAge);
+  });
   it('should test user age in seconds', function(){
     let testInput = new galactic('1990-04-21', 70, 28);
     let testDate = new Date(testInput.birthdate);
     let today = new Date();
-
     expect(testInput.ageInSeconds()).toEqual((Math.floor((today.getTime() - testDate.getTime()) / 1000)));
   });
-
   it('should test users age in earth years', function(){
     let testInput = new galactic('1990-04-21', 70, 28);
     expect(testInput.earthAge()).toEqual(28);
